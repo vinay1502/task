b288a5a53d4eb0876369d4d6af09af21ec18ff44
vinay1502 <vinaykuchi1502@gmail.com>
2017-01-13 14:29:16 -0500

    Update script.js

--- InterviewSample2/js/script.js
+++ InterviewSample2/js/script.js
@@ -1,31 +1,66 @@
 var app = angular.module("MyApp", []);
 
-app.controller("MyCtrl", ["$scope", function($scope) {
-    $scope.message = 'Hello World';
+app.controller("MyCtrl", ["$scope","$interval", function($scope,$interval) {
     
-    $scope.startRedLight = function () {
+    var duration = 0;
+    var sequenceIndex = 0;
+    var lights = [
+        {name:'G',duration:5},
+        {name:'Y',duration:2},
+        {name:'R',duration:5}
+    ];
+
+    $scope.isRedOn = false;
+    $scope.isYellowOn = false;
+    $scope.isGreenOn = false;
+
+    
+    function startRedLight() {
+         $scope.isRedOn = true;
+
+    }
+
+    function startYellowLight() {
+        $scope.isYellowOn = true;
+    }
+
+    function startGreenLight() {
+        $scope.isGreenOn = true;
+    }
+
+    function startLights(light){
+        resetLights();
+        if(light === 'R'){
+            startRedLight();
+        }else if(light === 'Y'){
+            startYellowLight();
+        }else  if(light === 'G'){
+            startGreenLight();
+        }
+    }
+
+    $interval(function(){
+        duration = duration+1;
+        if(duration <= lights[sequenceIndex].duration){
+            startLights(lights[sequenceIndex].name);
+        }
+        if(duration === lights[sequenceIndex].duration){
+            sequenceIndex = sequenceIndex+1;
+            duration = 0;
+        }
         
 
-    }
+        if(sequenceIndex >= lights.length){
+            sequenceIndex = 0;
+            duration = 0;
+        }
+       
+    },1000)
 
-    $scope.startYellowLight = function () {
-
-    }
-
-    $scope.startGreenLight = function () {
-
-    }
-
-    $scope.isDisplayRed = function () {
-        return true;
-    }
-
-    $scope.isDisplayYellow = function () {
-        return true;
-    }
-
-    $scope.isDisplayGreen = function () {
-        return true;
-    }
+   function resetLights(){
+        $scope.isRedOn = false;
+        $scope.isYellowOn = false;
+        $scope.isGreenOn = false;
+   }
 
 }]);
